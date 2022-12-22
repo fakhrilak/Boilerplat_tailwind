aab0860cfd23cec2f1e5c8f9ec29c66ee79e30ce
fakhrilak <fakhrilak23@gmail.com>
2022-12-22 17:28:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"
+import Navbar from './components/Navbar/Navbar'
 const App = () => {
   return (
     <div>
-      Hello world
+     <Router>
+        <Navbar>
+              {/* <Switch> */}
+              {/* <Route exact path="/" component={Home}/>
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+              <UserRoute exact path="/profile" component={Profile} to={"/login"}/>
+              <UserRoute exact path="/write/:id" component={Write} to={"/login"}/>
+              <UserRoute exact path="/music" component={Music} to={"/login"}/>
+              <AdminRoute exact path="/form" component={Form}/>
+              <Route exact path="/sub-category/:id" component={SubCategory}/>
+              <Route exact path="/content/:id" component={Read}/> */}
+              {/* </Switch> */}
+        </Navbar>
+      </Router>
     </div>
   )
 }
